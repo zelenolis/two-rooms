9e249a1217016039fa283add70b60789323f51dd
Evgenii Sukhanov <evgenii_sukhanov@epam.com>
2024-12-11 17:25:52 +0400

    feat: base routes added

--- two-rooms/src/app/app.routes.ts
+++ two-rooms/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        loadComponent: () => import("./components/dashboard/dashboard.component").then((mod) => mod.DashboardComponent)
+    },
+    {
+        path: "login",
+        loadComponent: () => import("./components/login/login.component").then((mod) => mod.LoginComponent)
+    }
+];
