485a0f1866bb8bd989ca278330173fe3ca6edf8e
Evgenii Sukhanov <evgenii_sukhanov@epam.com>
2024-12-11 17:25:39 +0400

    feat: base components created

---
+++ two-rooms/src/app/components/book/book.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book',
+  imports: [],
+  templateUrl: './book.component.html',
+  styleUrl: './book.component.scss'
+})
+export class BookComponent {
+
+}
