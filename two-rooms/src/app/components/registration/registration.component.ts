3ed563b0bd7dc1e917c626892555c756378eeae2
Evgenii Sukhanov <evgenii_sukhanov@epam.com>
2024-12-13 18:55:20 +0400

    feat: add register form template

--- two-rooms/src/app/components/registration/registration.component.ts
+++ two-rooms/src/app/components/registration/registration.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
 export class RegistrationComponent {
-
+  private readonly router = inject(Router)
+  
+  onSubmit(form: any) {
+    console.log(form.value)
+  }
+  onClick() {
+    this.router.navigate(["/registration"])
+  }
 }
