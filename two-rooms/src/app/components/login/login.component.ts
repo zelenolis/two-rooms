5660d02cbc7cc925603096c2ee077f8161d01317
Evgenii Sukhanov <evgenii_sukhanov@epam.com>
2024-12-13 18:55:06 +0400

    feat: add login form template

--- two-rooms/src/app/components/login/login.component.ts
+++ two-rooms/src/app/components/login/login.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  private readonly router = inject(Router)
 
+
+  onSubmit(form: any) {
+    console.log(form.value)
+  }
+  onClick() {
+    this.router.navigate(["/registration"])
+  }
 }
