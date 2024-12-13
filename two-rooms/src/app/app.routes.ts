19750b900b44fdbb49a53ab7c5ebbc27b2044e47
Evgenii Sukhanov <evgenii_sukhanov@epam.com>
2024-12-13 18:13:06 +0400

    feat: add guards to routes

--- two-rooms/src/app/app.routes.ts
+++ two-rooms/src/app/app.routes.ts
@@ -1,16 +1,21 @@
 import { Routes } from '@angular/router';
+import { isloggedGuard } from './guards/islogged.guard';
+import { notloggedGuard } from './guards/notlogged.guard';
 
 export const routes: Routes = [
     {
         path: "",
-        loadComponent: () => import("./components/dashboard/dashboard.component").then((mod) => mod.DashboardComponent)
+        loadComponent: () => import("./components/dashboard/dashboard.component").then((mod) => mod.DashboardComponent),
+        canActivate: [notloggedGuard]
     },
     {
         path: "login",
-        loadComponent: () => import("./components/login/login.component").then((mod) => mod.LoginComponent)
+        loadComponent: () => import("./components/login/login.component").then((mod) => mod.LoginComponent),
+        canActivate: [isloggedGuard]
     },
     {
         path: "registration",
-        loadComponent: () => import("./components/registration/registration.component").then((mod) => mod.RegistrationComponent)
+        loadComponent: () => import("./components/registration/registration.component").then((mod) => mod.RegistrationComponent),
+        canActivate: [isloggedGuard]
     }
 ];
