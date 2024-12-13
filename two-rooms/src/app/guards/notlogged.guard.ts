3905fd0166d542007bb2fad2654467ffb7f85908
Evgenii Sukhanov <evgenii_sukhanov@epam.com>
2024-12-13 18:09:32 +0400

    feat: add guards

---
+++ two-rooms/src/app/guards/notlogged.guard.ts
@@ -0,0 +1,8 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const notloggedGuard: CanActivateFn = (route, state) => {
+  const router: Router = inject(Router)
+  
+  return localStorage.getItem("logged") ? true : router.navigate(["/login"])
+};
