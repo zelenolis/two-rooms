bf82eda130f07db41fdfb0b73060e01826049996
Evgenii Sukhanov <evgenii_sukhanov@epam.com>
2024-12-16 18:42:26 +0400

    feat: add loader animation

--- two-rooms/src/app/components/login/login.component.ts
+++ two-rooms/src/app/components/login/login.component.ts
@@ -2,22 +2,27 @@ import { Component, inject } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { Router } from '@angular/router'
 import { LoginService } from '../../services/login.service'
+import { NgClass } from '@angular/common'
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, NgClass],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
   private readonly router = inject(Router)
   private readonly loginService = inject(LoginService)
+  hidden = true
 
   onSubmit(form: any) {
     console.log(form.value)
+    this.hidden = !this.hidden
+    /*
     this.loginService.getUsers().subscribe((res) => {
       console.log(res)
     })
+      */
   }
   onClick() {
     this.router.navigate(['/registration'])
