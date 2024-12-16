03b3d8da6653aade33cb3e7b15e072b49743f090
Evgenii Sukhanov <evgenii_sukhanov@epam.com>
2024-12-16 11:28:15 +0400

    feat: add connect to backend

--- two-rooms/src/app/services/login.service.ts
+++ two-rooms/src/app/services/login.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  constructor(private http: HttpClient) {}
+  private readonly backUrl = 'https://parseapi.back4app.com/classes/team'
 
-  constructor() { }
+  getUsers(): Observable<object> {
+    const headers = new HttpHeaders({
+      "First": "********",
+      "Second": "********"
+    })
+
+    return this.http.get(this.backUrl, {headers})
+  }
+
 }
