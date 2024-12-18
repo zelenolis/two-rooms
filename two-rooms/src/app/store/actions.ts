04734b01bcf7c90299202530768940e7c073ee13
Evgenii Sukhanov <evgenii_sukhanov@epam.com>
2024-12-18 12:46:12 +0400

    feat: add store actions

---
+++ two-rooms/src/app/store/actions.ts
@@ -0,0 +1,17 @@
+import { createAction, props } from "@ngrx/store";
+import { Booking } from "../interfaces/interfaces";
+
+export enum StoreActionsType {
+    addBook = '[ADD] add a booking',
+    addAllBooks = '[ADD] add many bookings',
+}
+
+export const addBookAction = createAction(
+    StoreActionsType.addBook,
+    props<{newBook: Booking}>()
+)
+
+export const addAllBooksAction = createAction(
+    StoreActionsType.addBook,
+    props<{newBooks: Booking[]}>()
+)
