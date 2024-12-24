99cf03aa1a942baf2b3fe837cdf6c536cb475fae
Evgenii Sukhanov <evgenii_sukhanov@epam.com>
2024-12-24 17:27:15 +0400

    feat: add date sort

---
+++ two-rooms/src/app/pipes/sort-by-date.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core'
+import { Booking } from '../interfaces/interfaces'
+
+@Pipe({
+  name: 'sortByDate'
+})
+export class SortByDatePipe implements PipeTransform {
+
+  transform(dates: Booking[], pickedDate: Date) {
+    if (!pickedDate) { return dates }
+    const formatDate = pickedDate.toISOString()
+    return dates.filter(val => val.date === formatDate)
+  }
+
+}
