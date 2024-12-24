2f0f83cf643a99f79bb3e79172a0a39c51dc9b14
Evgenii Sukhanov <evgenii_sukhanov@epam.com>
2024-12-24 18:25:31 +0400

    feat: add color tu datepicker

--- two-rooms/src/app/components/book/book.component.ts
+++ two-rooms/src/app/components/book/book.component.ts
@@ -1,28 +1,31 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { ChangeDetectionStrategy, Component, model, ViewEncapsulation } from '@angular/core'
 import {MatFormFieldModule} from '@angular/material/form-field'
 import {MatTimepickerModule} from '@angular/material/timepicker'
 import {MatInputModule} from '@angular/material/input'
 import {MatDatepickerModule} from '@angular/material/datepicker'
 import { FormsModule } from '@angular/forms'
+import { MatCardModule } from '@angular/material/card'
 
 
 @Component({
   selector: 'app-book',
-  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, FormsModule, MatTimepickerModule],
+  imports: [MatCardModule, MatInputModule, MatDatepickerModule, FormsModule, MatTimepickerModule],
   templateUrl: './book.component.html',
   styleUrl: './book.component.scss',
+  encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BookComponent {
-  selectedDate: Date | undefined
+  selected = model<Date | null>(null)
   selectedTime: string | undefined
 
   dateChanged() {
-    if (this.selectedDate && this.selectedTime) {
+    const pick = this.selected()
+    if (pick && this.selectedTime) {
       const t = new Date(this.selectedTime)
       const hours = t.getHours().toString().padStart(2, '0')
       const min = t.getMinutes().toString().padStart(2, '0')
-      const d = new Date(this.selectedDate)
+      const d = new Date(pick)
       const year = d.getFullYear()
       const month = d.getMonth()
       const day = d.getDay()
