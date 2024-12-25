836c7b8db675a64f4a42e36c398224f25d5ffee9
Evgenii Sukhanov <evgenii_sukhanov@epam.com>
2024-12-25 14:42:22 +0400

    feat: add selecting book options

--- two-rooms/src/app/components/book/book.component.ts
+++ two-rooms/src/app/components/book/book.component.ts
@@ -1,21 +1,38 @@
 import { ChangeDetectionStrategy, Component, model, ViewEncapsulation } from '@angular/core'
 import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatSelectModule} from '@angular/material/select'
 import {MatTimepickerModule} from '@angular/material/timepicker'
 import {MatInputModule} from '@angular/material/input'
 import {MatDatepickerModule} from '@angular/material/datepicker'
 import { FormsModule } from '@angular/forms'
 import { MatCardModule } from '@angular/material/card'
+import { NgFor } from '@angular/common'
 
 
 @Component({
   selector: 'app-book',
-  imports: [MatCardModule, MatInputModule, MatDatepickerModule, FormsModule, MatTimepickerModule],
+  imports: [
+    NgFor,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatCardModule, 
+    MatInputModule, 
+    MatDatepickerModule, 
+    FormsModule, 
+    MatTimepickerModule],
   templateUrl: './book.component.html',
   styleUrl: './book.component.scss',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BookComponent {
+  protected repeates: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  protected yourBookIs = 'Please select date and time'
+  protected repeatOption = ''
+  protected repeatTimes = 0
+  protected showRepeats = ''
+  protected room = 'any'
+
   selected = model<Date | null>(null)
   selectedTime: string | undefined
 
@@ -29,7 +46,31 @@ export class BookComponent {
       const year = d.getFullYear()
       const month = d.getMonth()
       const day = d.getDay()
-      console.log(` ${hours}:${min}, ${day}.${month}.${year}`)
+      this.yourBookIs = `Your book is: ${hours}:${min}, ${day}.${month}.${year}`
+      console.log(`${hours}:${min}, ${day}.${month}.${year}`)
     }
   }
+
+  rooms(val: string) {
+    this.room = val
+  }
+
+  timeRepeat(val: number) {
+    this.repeatTimes = val
+    this.checkRepeats()
+  }
+
+  repeats(val: string) {
+    this.repeatOption = val
+    this.checkRepeats()
+  }
+
+  checkRepeats() {
+    if (this.repeatTimes > 0 && this.repeatOption && this.repeatOption !== 'no repeat') {
+      this.showRepeats = `Repeat every ${this.repeatOption} ${this.repeatTimes} times`
+    } else {
+      this.showRepeats = ''
+    }
+  }
+
 }
