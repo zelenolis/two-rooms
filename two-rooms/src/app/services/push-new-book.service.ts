21c2ced96753b3e8bc965ec378edcc4288ad4814
Evgenii Sukhanov <evgenii_sukhanov@epam.com>
2024-12-26 11:54:21 +0400

    feat: add send book to store

--- two-rooms/src/app/services/push-new-book.service.ts
+++ two-rooms/src/app/services/push-new-book.service.ts
@@ -1,9 +1,11 @@
 import { HttpClient } from '@angular/common/http'
 import { inject, Injectable } from '@angular/core'
-import { SendBooking } from '../interfaces/interfaces'
-import { from, mergeMap, toArray } from 'rxjs'
+import { Booking, SendBooking } from '../interfaces/interfaces'
+import { from, mergeMap, tap, toArray } from 'rxjs'
 import { MatSnackBar } from '@angular/material/snack-bar'
 import { Router } from '@angular/router'
+import { Store } from '@ngrx/store'
+import { addBookAction } from '../store/actions'
 
 @Injectable({
   providedIn: 'root',
@@ -11,19 +13,25 @@ import { Router } from '@angular/router'
 export class PushNewBookService {
   private readonly matSnackBar = inject(MatSnackBar)
   private readonly router = inject(Router)
+  private readonly store = inject(Store)
   private readonly storeUrl = 'https://parseapi.back4app.com/classes/booking'
 
   constructor(private http: HttpClient) {}
 
-  pushRequest(data: SendBooking[]) {
+  pushRequest(data: Booking[]) {
     from(data)
       .pipe(
-        mergeMap((item) => this.http.post(this.storeUrl, JSON.stringify(item))),
+        mergeMap((item) => this.http.post<{ objectId: string }>(this.storeUrl, JSON.stringify(item)).pipe(
+          tap(res => {
+            item.objectId = res.objectId
+            this.store.dispatch(addBookAction({newBook: item}))
+          })
+        )
+        ),
         toArray(),
       )
       .subscribe({
         next: (res) => {
-          console.log(res)
           this.matSnackBar.open('Date(s) succesfully booked', 'OK', {
             duration: 3000,
           })
