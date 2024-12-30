1480809af7c0846340ba1fcbc59bb8e90070f877
Evgenii Sukhanov <evgenii_sukhanov@epam.com>
2024-12-30 17:14:13 +0400

    feat: insert popup confirm

--- two-rooms/src/app/components/reservation/reservation.component.ts
+++ two-rooms/src/app/components/reservation/reservation.component.ts
@@ -1,14 +1,18 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, Input } from '@angular/core';
 import { Booking } from '../../interfaces/interfaces';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ConfirmPopupComponent } from '../confirm-popup/confirm-popup.component';
 
 @Component({
   selector: 'app-reservation',
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './reservation.component.html',
   styleUrl: './reservation.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ReservationComponent {
+  private readonly matDialog = inject(MatDialog)
+
   @Input()
   pickedDate: Booking = {
     objectId: '',
@@ -18,4 +22,18 @@ export class ReservationComponent {
     duration: '',
     room: '',
   };
+
+  onDelete() {
+    const buttonElement = document.activeElement as HTMLElement;
+    buttonElement.blur();
+    const dialogRef = this.matDialog.open(ConfirmPopupComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log('true')
+      } else {
+        console.log('false')
+      }
+    });
+  }
+
 }
