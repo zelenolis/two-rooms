43aca47ae9f315a2324dc065f3d1f5a6bea30818
Evgenii Sukhanov <evgenii_sukhanov@epam.com>
2024-12-30 17:13:47 +0400

    feat: add popup confirm

---
+++ two-rooms/src/app/components/confirm-popup/confirm-popup.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirm-popup',
+  imports: [MatDialogModule, MatButtonModule, MatDialogTitle],
+  templateUrl: './confirm-popup.component.html',
+  styleUrl: './confirm-popup.component.scss'
+})
+export class ConfirmPopupComponent {
+  private matDialogRef = inject(MatDialogRef)
+
+  onConfirm() {
+    this.matDialogRef.close(true);
+  }
+
+  onCancel() {
+    this.matDialogRef.close(false);
+  }
+
+}
